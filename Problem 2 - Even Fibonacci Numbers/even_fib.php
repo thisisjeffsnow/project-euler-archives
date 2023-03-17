8a3e4a97b80433025fe511a34a4e44381cfc3560
Jeff Snow <thisisjeffsnow@gmail.com>
2023-03-17 10:47:04 -0400

    Adds Problem #2

---
+++ Problem 2 - Even Fibonacci Numbers/even_fib.php
@@ -0,0 +1,38 @@
+<?php
+
+
+class Fibonacci
+{
+    public static function evenFib(int $n): int
+    {
+        /* Using formula for Nth Fibonacci number */
+        $bigPhi = (1 + sqrt(5))/2;
+        $smallPhi = (1 - sqrt(5))/2;
+
+        return (int) ((pow($bigPhi, 3*$n) - pow($smallPhi, 3*$n))/sqrt(5));
+    }
+}
+
+$sum = 0;
+$index = 0;
+
+/* Problem says only add even terms that are less than 4 million. */
+$limit = 3999999;
+
+while(true)
+{
+    /* Pump out even Fibonacci numbers and add to our total
+    until we exceed 4 million */
+    $currentEvenFib = Fibonacci::evenFib($index);
+    if($currentEvenFib < $limit) {
+        $sum += $currentEvenFib;
+        $index++;
+    } else {
+        break;
+    }
+}
+
+echo "S = $sum";
+
+
+
